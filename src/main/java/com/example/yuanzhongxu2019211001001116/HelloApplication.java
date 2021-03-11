b346c2f670a2392bdcca3dc8aecb2ae846fb7c10
[yuanzhongxu] <[3295902458@qq.com]>
2021-03-11 11:21:22 +0800

    First commit

---
+++ src/main/java/com/example/yuanzhongxu2019211001001116/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.yuanzhongxu2019211001001116;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
